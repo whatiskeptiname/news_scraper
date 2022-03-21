783695f7c7a1e14eb18691e5fb0e3d3013198801
whatiskeptiname <susangmree@gmail.com>
2022-03-21 20:47:59 +0545

    cleaned

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-Scraps News from Aanapurna.com.
+Scraps News from Aanapurna.com containing the given word.
+
+Use Prettify JSON extension to read the JSON file in VS code.
--- main.py
+++ main.py
@@ -1,7 +1,10 @@
+import imp
 import requests
 from requests.exceptions import HTTPError
 import json
 import re
+import os
+from pathlib import Path
 
 
 def cleanhtml(raw_html):  # remove html tags from articles
@@ -13,12 +16,22 @@ def cleanhtml(raw_html):  # remove html tags from articles
 
 search_title = "हाम्रो"  # title to search
 cleaned_articles = []  # list of articles
+previous_articles = []  # list of articles already downloaded
+page_count = 1
 try:
-    # with open("pagination_counter.json","w+") as pf:
-    #     pagination_i = int(pf.readline())
-    # print("-------------------" + pagination_i)
-    for page in range(
-        5, 6
+    # existing_file = Path(f"{search_title}.json")
+    # existing_file.touch(exist_ok=True)
+
+    # with open(f"{search_title}.json", "r") as f:  # keeping search_title as file name
+    #     article_count = (
+    #         len(f.readlines()) - 2
+    #     )  # -2 because of the last brackets "[]" in the list
+
+    # page_count = (article_count // 10) + 1  # 10 articles per page
+    # if page_count < 1:
+    #     page_count = 1
+    for page in range(  # starting from page_count (current page) next remaning pages
+        1, 4
     ):  # each page contains 10 articles so taking 3 pages in loop for 30 articles
         url = (
             "https://bg.annapurnapost.com/api/search?title="
@@ -36,13 +49,20 @@ try:
             cleaned_articles.append(cleaned_article)
             i = i + 1
 
-    with open(f"{search_title}.json", "w") as f:  # keeping search_title as file name
+    # with open(f"{search_title}.json", "w+") as f:  # keeping search_title as file name
+    #     if os.stat(f"{search_title}.json").st_size == 0:
+    #         json.dump(cleaned_articles, f)
+    #         print("new file -------------------")
+    #     previous_articles = json.loads(f.read())
+    #     final_articles = previous_articles + cleaned_articles
+    #     json.dump(cleaned_articles, f)
+
+    with open(f"{search_title}.json", "w+") as f:  # keeping search_title as file name
         json.dump(cleaned_articles, f)
 
 except HTTPError as http_err:
     print(f"HTTP error occurred: {http_err}")
 except Exception as err:
     print(f"Other error occurred: {err}")
-
 
 print("Completed!!!")
